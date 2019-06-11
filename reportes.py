18f64f7eb166f847818a4dc5d29a27a7ffe7dca5
Aaron Cuadras <aaron.cuadras01@utrgv.edu>
2019-06-10 22:34:14 -0500

    Finished report creation script

---
+++ reportes.py
@@ -0,0 +1,231 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import csv, pyodbc
+
+MDB = 'c:\SetupPV\Data\DBTRUCK.mdb'
+DRV = '{Microsoft Access Driver (*.mdb)}'
+PWD = 'pw'
+
+con = pyodbc.connect('DRIVER={};DBQ={};PWD={}'.format(DRV,MDB,PWD))
+cur = con.cursor()
+
+#Client query selector
+SQL = 'SELECT NOMBRE FROM cliente;'
+clientes = cur.execute(SQL).fetchall()
+
+
+#Tkinter kit creator
+top = Tk()
+top.title("Seleccionar Cliente")
+frame = Frame(top)
+frame.grid(columnspan=2)
+label2 = Label(frame, text="Cliente")
+label2.grid()
+
+##Start query list
+list1 = Listbox(frame, selectmode=SINGLE, height=15)
+counter = 0
+for rows in clientes:
+    counter =+ 1
+    for el in rows:
+        list1.insert(counter, el)
+list1.grid(column=1, row=0)
+##End query list
+
+
+
+'''Creates new windows with information for the specific client'''
+def tablaCliente(cl, value):
+
+    def displayQuery(a):
+        curItem = chat.focus()
+        curTruck = chat.item(curItem)
+       # print (curTruck['values'][0])
+        info1.delete(0,END)
+        info1.insert(0, str(curTruck['values'][0]))
+        info2.delete(0,END)
+        info2.insert(0, str(curTruck['values'][1]))
+        SQL = "SELECT embarque FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
+        info3.delete(0, END)
+        econ = cur.execute(SQL).fetchall()
+        info3.insert(0, str(econ[0][0]))
+        info4.delete(0,END)
+        info4.insert(0, str(curTruck['values'][2]))
+        SQL = "SELECT guia FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
+        econ = cur.execute(SQL).fetchall()
+        info5.delete(0,END)
+        info5.insert(0, str(econ[0][0]))
+        SQL = "SELECT PROVEEDOR FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
+        econ = cur.execute(SQL).fetchall()
+        info6.delete(0,END)
+        info6.insert(0, str(econ[0][0]))
+
+
+    def save_client():
+        #If all entries have data, save the cliente. Otherwise display an error message
+        if(info1.get() != "" and info2.get() != "" and info4.get() != "" and info5.get() != "" and info6.get() != ""):
+            SQL = "UPDATE HISTORIA SET guia='" + str(info5.get()) + "', PROVEEDOR='" + str(info6.get()) + "' WHERE FECHA1='" + str(info1.get()) + "' AND PLACAS='" + str(info2.get()) + "' AND observacion='" + str(info4.get()) + "';"
+            cur.execute(SQL)
+            cur.commit()
+            messagebox.showinfo("Creado", "Folio y cliente creado")
+        else:
+            messagebox.showerror("Error", "Llenar todos los campos")
+
+    def del_entries():
+        info1.delete(0, END)
+        info2.delete(0, END)
+        info3.delete(0, END)
+        info4.delete(0, END)
+        info5.delete(0, END)
+        info6.delete(0, END)
+        
+
+    def filter_date():
+        SQL = "SELECT * FROM HISTORIA WHERE FECHA1 BETWEEN '" + str(entry1.get()) + "' AND '" + str(entry2.get()) + "' AND CLIENTE=" + str(cl) + ";"
+        query = cur.execute(SQL).fetchall()
+        for i in chat.get_children():
+            chat.delete(i)
+
+        for row in query:
+            chat.insert('',1, text = row[1], values =(row[1],row[3], row[32]))
+        
+    
+    def generate_report():
+        def create_csv():
+            SQL = "SELECT FECHA1, PROVEEDOR, FOLIO, embarque, observacion, PLACAS, guia, PRODUCTO FROM HISTORIA WHERE FECHA1='" + str(date.get()) + "' AND CLIENTE=" + str(cl) + ";"
+            rows = cur.execute(SQL).fetchall()
+            if(cl != 3):
+                totalItems = 0
+                for i in rows:
+                    i[7] = "$280.00"
+                    totalItems += 1
+            else:
+                for i in rows:
+                    i[7] = "CANCELADO"
+                
+            with open('REPORTE_DIARIO.csv', 'w', newline='') as fou:
+                csv_writer = csv.writer(fou) # default field-delimiter is ","
+                csv_writer.writerow(['FECHA', 'CLIENTE/DESTINO', 'FACTURA/FOLIO', 'ECONOMICO', 'ALPHA', 'PLACAS', 'FOLIO BASCULA', '$'])
+                csv_writer.writerows(rows)
+                if(cl != 3):
+                    csv_writer.writerow(['','','','','','TOTAL',str(totalItems), "$" + str(280 * totalItems) + ".00"])
+            messagebox.showinfo("Creado", "Reporte Creado")
+            r.destroy()
+
+
+        r = Toplevel(tableWindow)
+        r.title('Crear Reporte')
+        label11 = Label(r, text="Ingresar Fecha [DD/MM/AAAA]:")
+        label11.pack()
+        date = Entry(r)
+        date.pack()
+        submit = Button(r, text="Seleccionar", command=create_csv)
+        submit.pack()
+
+    tableWindow = Toplevel(top)
+    tableWindow.title("Cliente: " + value)
+    tableWindow.geometry("800x390")
+    
+    label2 = Label(tableWindow, text="iSTOP")
+    label2.place(x=5, y=5)
+    chat = ttk.Treeview(tableWindow, height="15", columns=("Nick","Mensaje","Hora"), selectmode="extended")
+    chat.place(x=5, y=60)
+    chat.heading('#1', text='Fecha', anchor=W)
+    chat.heading('#2', text='Placas', anchor=W)
+    chat.heading('#3', text='Alpha', anchor=W)
+    chat.column('#1', stretch=NO, minwidth=0, width=130)
+    chat.column('#2', stretch=NO, minwidth=0, width=65)
+    chat.column('#3', stretch=NO, minwidth=0, width=65)
+    chat.column('#0', stretch=NO, minwidth=0, width=0)
+    chat.bind("<Double-1>", displayQuery)
+
+    SQL = "SELECT * FROM HISTORIA WHERE CLIENTE=" + str(cl) + ";"
+    query = cur.execute(SQL).fetchall()
+    for row in query:
+        chat.insert('',1, text = row[1], values =(row[1],row[3], row[32]))
+
+    label9 = Label(tableWindow, text="Desde [DD/MM/AAAA]:")
+    label9.place(x=5, y=30)
+    entry1 = Entry(tableWindow)
+    entry1.place(x=140, y=30)
+    label10 = Label(tableWindow, text="Hasta: [DD/MM/AAAA]:")
+    label10.place(x=240, y=30)
+    entry2 = Entry(tableWindow)
+    entry2.place(x=375, y=30)
+    buttonDate = Button(tableWindow, command=filter_date, text="Filtrar por Fecha")
+    buttonDate.place(x=500, y=30)
+    
+    label3 = Label(tableWindow, text='Fecha')
+    label3.place(x=350, y=80)
+    info1 = Entry(tableWindow)
+    info1.place(x=390, y=80)
+    label4 = Label(tableWindow, text="Placas")
+    label4.place(x=550, y=80)
+    info2 = Entry(tableWindow)
+    info2.place(x=590, y=80)
+    label5 = Label(tableWindow, text="Economico")
+    label5.place(x=350, y=120)
+    info3 = Entry(tableWindow)
+    info3.place(x=420, y=120)
+    label6 = Label(tableWindow, text="Alpha")
+    label6.place(x=550, y=120)
+    info4 = Entry(tableWindow)
+    info4.place(x=590, y=120)
+    label7 = Label(tableWindow, text="Folio")
+    label7.place(x=350, y=160)
+    info5 = Entry(tableWindow)
+    info5.place(x=390, y=160)
+    label8 = Label(tableWindow, text="Cliente/Destino")
+    label8.place(x=550, y=160)
+    info6 = Entry(tableWindow)
+    info6.place(x=650, y=160)
+
+    buttonSave = Button(tableWindow, command=save_client, text="Guardar")
+    buttonSave.place(x=400, y=300)
+
+    buttonClear =Button(tableWindow, text="Borrar Campos", command=del_entries)
+    buttonClear.place(x=480, y=300)
+
+    buttonReport = Button(tableWindow, text="Generar Reporte", command=generate_report)
+    buttonReport.place(x=600, y=300)
+
+    
+    
+#Display element from the query where the client is the one selected
+#and has the values for 'Fecha' and 'Placas'
+
+def selecc():
+    value = str(list1.get(ACTIVE))
+    SQL = "SELECT numero FROM cliente WHERE nombre='" + value + "';"
+    s = cur.execute(SQL).fetchall()
+    print("Seleccionando cliente: " + value)
+    tablaCliente(s[0][0], value)
+
+
+def create_cancel():
+    def cancel_folio():
+        SQL = "UPDATE HISTORIA SET PROVEEDOR='CANCELADO', FOLIO=00, embarque='CANCELADO', observacion='CANCELADO', PLACAS='CANCELADO', CLIENTE=3 WHERE guia='" + str(entry4.get()) + "';"
+        cur.execute(SQL)
+        cur.commit()
+        messagebox.showinfo("Cancelado", "Folio Cancelado")
+        cancelar.destroy()
+
+    cancelar = Toplevel(top)
+    cancelar.title('Cancelar Folio')
+    label12 = Label(cancelar, text="Ingresar Folio Bascula a Cancelar")
+    label12.pack()
+    entry4 = Entry(cancelar)
+    entry4.pack()
+    submit_cancel = Button(cancelar, text="Cancelar", command=cancel_folio)
+    submit_cancel.pack()
+
+
+button1 = Button(frame, text="Seleccionar", command=selecc)
+button1.grid(row=1)
+button2 = Button(frame, text="Cancelar Folio", command=create_cancel)
+button2.grid(row=1, column=1)
+
+top.mainloop()
+cur.close()
+con.close()
