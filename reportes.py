02805e232589458cb9ad3caf20d9f705660cceb8
Aaron Cuadras <aaron.cuadras01@utrgv.edu>
2019-06-21 12:50:56 -0500

    Reports added

--- reportes.py
+++ reportes.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import csv, pyodbc
 
-MDB = 'c:\SetupPV\Data\DBTRUCK.mdb'
+MDB = 'c:\SetupPV\Data\DBTRUCK.mdb' 
 DRV = '{Microsoft Access Driver (*.mdb)}'
 PWD = 'pw'
 
@@ -37,11 +37,11 @@ list1.grid(column=1, row=0)
 
 '''Creates new windows with information for the specific client'''
 def tablaCliente(cl, value):
-
+    
     def displayQuery(a):
         curItem = chat.focus()
         curTruck = chat.item(curItem)
-       # print (curTruck['values'][0])
+
         info1.delete(0,END)
         info1.insert(0, str(curTruck['values'][0]))
         info2.delete(0,END)
@@ -52,20 +52,33 @@ def tablaCliente(cl, value):
         info3.insert(0, str(econ[0][0]))
         info4.delete(0,END)
         info4.insert(0, str(curTruck['values'][2]))
-        SQL = "SELECT guia FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
+        SQL = "SELECT FOLIOGEN FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
         econ = cur.execute(SQL).fetchall()
         info5.delete(0,END)
-        info5.insert(0, str(econ[0][0]))
+        if(econ[0][0] != None):
+            info5.insert(0, str(econ[0][0]))
         SQL = "SELECT PROVEEDOR FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
         econ = cur.execute(SQL).fetchall()
         info6.delete(0,END)
-        info6.insert(0, str(econ[0][0]))
+        if(econ[0][0] != None):
+            info6.insert(0, str(econ[0][0]))
+        SQL = "SELECT guia FROM HISTORIA WHERE PLACAS='" + str(curTruck['values'][1]) + "' AND observacion='" + str(curTruck['values'][2]) +"' AND FECHA1='" + str(curTruck['values'][0]) + "';"
+        econ = cur.execute(SQL).fetchall()
+        info7.delete(0,END)
+        if(econ[0][0] != None):
+            info7.insert(0, str(econ[0][0]))
+        SQL= "SELECT * FROM PERMISOS WHERE FOLIOBAS='" + str(info7.get()) + "';"
+        per = cur.execute(SQL).fetchall()
+        if(per == []):
+            permit1.deselect()
+        else:
+            permit1.select()
 
-
+    
     def save_client():
         #If all entries have data, save the cliente. Otherwise display an error message
-        if(info1.get() != "" and info2.get() != "" and info4.get() != "" and info5.get() != "" and info6.get() != ""):
-            SQL = "UPDATE HISTORIA SET guia='" + str(info5.get()) + "', PROVEEDOR='" + str(info6.get()) + "' WHERE FECHA1='" + str(info1.get()) + "' AND PLACAS='" + str(info2.get()) + "' AND observacion='" + str(info4.get()) + "';"
+        if(info1.get() != "" and info2.get() != "" and info4.get() != "" and info5.get() != "" and info6.get() != "" and info7.get() != ""):
+            SQL = "UPDATE HISTORIA SET guia='" + str(info7.get()) + "', PROVEEDOR='" + str(info6.get()) + "', FOLIOGEN='" + str(info5.get()) + "' WHERE FECHA1='" + str(info1.get()) + "' AND PLACAS='" + str(info2.get()) + "' AND observacion='" + str(info4.get()) + "';"
             cur.execute(SQL)
             cur.commit()
             messagebox.showinfo("Creado", "Folio y cliente creado")
@@ -79,23 +92,32 @@ def tablaCliente(cl, value):
         info4.delete(0, END)
         info5.delete(0, END)
         info6.delete(0, END)
+        info7.delete(0, END)
         
 
     def filter_date():
-        SQL = "SELECT * FROM HISTORIA WHERE FECHA1 BETWEEN '" + str(entry1.get()) + "' AND '" + str(entry2.get()) + "' AND CLIENTE=" + str(cl) + ";"
-        query = cur.execute(SQL).fetchall()
-        for i in chat.get_children():
-            chat.delete(i)
+        if(entry1.get() != '' and entry2.get() != ''):
+            SQL = "SELECT * FROM HISTORIA WHERE FECHA1 BETWEEN '" + str(entry1.get()) + "' AND '" + str(entry2.get()) + "' AND CLIENTE=" + str(cl) + ";"
+            query = cur.execute(SQL).fetchall()
+            for i in chat.get_children():
+                chat.delete(i)
 
-        for row in query:
-            chat.insert('',1, text = row[1], values =(row[1],row[3], row[32]))
+            for row in query:
+                chat.insert('',1, text = row[1], values =(row[1],row[3], row[32]))
+        else:
+            for i in chat.get_children():
+                chat.delete(i)
+            SQL = "SELECT * FROM HISTORIA WHERE CLIENTE=" + str(cl) + ";"
+            query = cur.execute(SQL).fetchall()
+            for row in query:
+                chat.insert('',1, text = row[1], values =(row[1],row[3], row[32]))
         
     
     def generate_report():
         def create_csv():
             SQL = "SELECT FECHA1, PROVEEDOR, FOLIO, embarque, observacion, PLACAS, guia, PRODUCTO FROM HISTORIA WHERE FECHA1='" + str(date.get()) + "' AND CLIENTE=" + str(cl) + ";"
             rows = cur.execute(SQL).fetchall()
-            if(cl != 3):
+            if(cl != 4):
                 totalItems = 0
                 for i in rows:
                     i[7] = "$280.00"
@@ -108,7 +130,7 @@ def tablaCliente(cl, value):
                 csv_writer = csv.writer(fou) # default field-delimiter is ","
                 csv_writer.writerow(['FECHA', 'CLIENTE/DESTINO', 'FACTURA/FOLIO', 'ECONOMICO', 'ALPHA', 'PLACAS', 'FOLIO BASCULA', '$'])
                 csv_writer.writerows(rows)
-                if(cl != 3):
+                if(cl != 4):
                     csv_writer.writerow(['','','','','','TOTAL',str(totalItems), "$" + str(280 * totalItems) + ".00"])
             messagebox.showinfo("Creado", "Reporte Creado")
             r.destroy()
@@ -122,6 +144,55 @@ def tablaCliente(cl, value):
         date.pack()
         submit = Button(r, text="Seleccionar", command=create_csv)
         submit.pack()
+
+
+    def show_permit():
+        def save_permit():
+            SQL= "SELECT * FROM PERMISOS WHERE FOLIOBAS='" + str(info7.get()) + "';"
+            test = cur.execute(SQL).fetchall()
+            #If the id is not found in the table
+            if(test == []):
+                SQL = "INSERT INTO PERMISOS (FOLIOBAS, FOLIO, NOMBRE, TIPO) VALUES ('" + str(info7.get()) + "', '" + str(permitIDInfo.get()) \
+                    + "', '" + str(permitNameInfo.get()) + "', '" + str(permitTypeInfo.get()) + "');" 
+                cur.execute(SQL)  
+                cur.commit()
+            else:
+                SQL = "UPDATE PERMISOS SET FOLIO='" + str(permitIDInfo.get()) + "', NOMBRE='" + str(permitNameInfo.get()) + "', TIPO='" + str(permitTypeInfo.get()) + "' WHERE FOLIOBAS='" + str(info7.get()) + "';"
+                cur.execute(SQL)
+                cur.commit()
+
+        if(permitCheck.get() == 1 and info7.get() != ''):
+            P = Toplevel(tableWindow)
+            P.title('Informacion de Permiso')
+            P.geometry("300x100")
+            ti = Label(P)
+            ti.grid(columnspan=4)
+            permitID = Label(P, text='Número: ')
+            permitID.grid(column=0, row=0)
+            permitIDInfo = Entry(P)
+            permitIDInfo.grid(column=1, row=0)
+            permitName = Label(P, text='Nombre: ')
+            permitName.grid(column=0, row=1)
+            permitNameInfo = Entry(P, width=35)
+            permitNameInfo.grid(column=1, row=1)
+            permitType = Label(P, text="Tipo: ")
+            permitType.grid(column=0, row=2)
+            permitTypeInfo = Entry(P, width=25)
+            permitTypeInfo.grid(column=1, row=2)
+            saveBtn = Button(P, text="Guardar", command=save_permit)
+            saveBtn.grid(column=1)
+
+            SQL = "SELECT * FROM PERMISOS WHERE FOLIOBAS='" + str(info7.get()) + "';"
+            permit = cur.execute(SQL).fetchall()
+            permitIDInfo.delete(0, END)
+            permitNameInfo.delete(0, END)
+            permitTypeInfo.delete(0, END)
+            if(permit != []):
+                permitIDInfo.insert(0, permit[0][1])
+                permitNameInfo.insert(0, permit[0][2])
+                permitTypeInfo.insert(0, permit[0][3])
+        else:
+            messagebox.showerror('Error', 'Permiso no habilitado')
 
     tableWindow = Toplevel(top)
     tableWindow.title("Cliente: " + value)
@@ -180,15 +251,25 @@ def tablaCliente(cl, value):
     label8.place(x=550, y=160)
     info6 = Entry(tableWindow)
     info6.place(x=650, y=160)
+    label13 = Label(tableWindow, text="Folio Bascula")
+    label13.place(x=400, y=200)
+    info7 = Entry(tableWindow)
+    info7.place(x=480, y=200)
+    permitCheck = IntVar()
+    permit1 = Checkbutton(tableWindow, text="Permiso", variable=permitCheck)
+    permit1.place(x=480, y=230)
 
     buttonSave = Button(tableWindow, command=save_client, text="Guardar")
-    buttonSave.place(x=400, y=300)
+    buttonSave.place(x=350, y=300)
 
     buttonClear =Button(tableWindow, text="Borrar Campos", command=del_entries)
-    buttonClear.place(x=480, y=300)
+    buttonClear.place(x=430, y=300)
 
     buttonReport = Button(tableWindow, text="Generar Reporte", command=generate_report)
-    buttonReport.place(x=600, y=300)
+    buttonReport.place(x=550, y=300)
+
+    buttonPermit = Button(tableWindow, text="Info. Permiso", command=show_permit)
+    buttonPermit.place(x=670, y=300)
 
     
     
@@ -205,7 +286,7 @@ def selecc():
 
 def create_cancel():
     def cancel_folio():
-        SQL = "UPDATE HISTORIA SET PROVEEDOR='CANCELADO', FOLIO=00, embarque='CANCELADO', observacion='CANCELADO', PLACAS='CANCELADO', CLIENTE=3 WHERE guia='" + str(entry4.get()) + "';"
+        SQL = "UPDATE HISTORIA SET PROVEEDOR='CANCELADO', FOLIO=00, embarque='CANCELADO', observacion='CANCELADO', PLACAS='CANCELADO', CLIENTE=4 WHERE guia='" + str(entry4.get()) + "';"
         cur.execute(SQL)
         cur.commit()
         messagebox.showinfo("Cancelado", "Folio Cancelado")
